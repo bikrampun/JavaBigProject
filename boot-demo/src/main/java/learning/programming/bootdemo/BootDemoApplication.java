68762373fc1ad2347952dc706c4436baf3bdc496
Bikram Pun <bikrampun47@gmail.com>
2022-09-29 12:18:09 +0545

    spring boot demo

---
+++ boot-demo/src/main/java/learning/programming/bootdemo/BootDemoApplication.java
@@ -0,0 +1,13 @@
+package learning.programming.bootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDemoApplication.class, args);
+	}
+
+}
