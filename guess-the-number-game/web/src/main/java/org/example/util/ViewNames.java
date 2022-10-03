8bbad90421c3605a5acc448f5ea69bc0dcc10d57
Bikram Pun <bikrampun47@gmail.com>
2022-10-03 21:32:03 +0545

    thymeleaf(template engine framework) used and spring-boot-devtools used

---
+++ guess-the-number-game/web/src/main/java/org/example/util/ViewNames.java
@@ -0,0 +1,10 @@
+package org.example.util;
+
+public final class ViewNames {
+
+    //== constants
+    public static final String HOME = "home";
+
+    //== constructors
+    private ViewNames() {}
+}
